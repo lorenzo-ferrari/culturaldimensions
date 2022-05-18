eb5d2a6ba1cb6e93e695ec2923e21077a9483b12
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2022-05-18 08:52:53 +0200

    first working version

---
+++ website/questions.js
@@ -0,0 +1,266 @@
+questions = [
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have sufficient time for your personal or home life",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have a boss (direct superior) you can respect",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"get recognition for good performance",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have a security of employment",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have pleasant people to work with",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"do work that is interesting",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"be consulted by your boss in decisions involving your work",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"live in a desirable area",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have a job respected by your family and friends",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"Think of an ideal job. How important is it to you to...",
+    "text":"have chances for promotion",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"In your private life, how important is to you...",
+    "text":"keeping time free for fun",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"In your private life, how important is to you...",
+    "text":"moderation: having few desires",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"In your private life, how important is to you...",
+    "text":"doing a service to a friend",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"In your private life, how important is to you...",
+    "text":"thrift (not spending more than needed)",
+    "options":[
+      "of utmost importance",
+      "very important",
+      "of moderate importance",
+      "of little importance",
+      "of very little or no importance"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"How often do you feel nervous or tense?",
+    "options":[
+      "always",
+      "usually",
+      "sometimes",
+      "seldom",
+      "never"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"Are you a happy person?",
+    "options":[
+      "always",
+      "usually",
+      "sometimes",
+      "seldom",
+      "never"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"Do other people or circumstances ever prevent you from doing what you really want to?",
+    "options":[
+      "yes, always",
+      "yes, usually",
+      "sometimes",
+      "no, seldom",
+      "no, never"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"All in all, how would you describe your state of health these days?",
+    "options":[
+      "very good",
+      "good",
+      "fair",
+      "poor",
+      "very poor"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"How proud are you to be a citizen of your country?",
+    "options":[
+      "very proud",
+      "fairly proud",
+      "somewhat proud",
+      "not very proud",
+      "not proud at all"
+    ]
+  },
+  {
+    "intro":"",
+    "text":"How often, in your experience, are subordinates afraid to contradict their boss (or students their teacher)?",
+    "options":[
+      "always",
+      "usually",
+      "sometimes",
+      "seldom",
+      "never"
+    ]
+  },
+  {
+    "intro":"To what extent do you agree or disagree with the following statement?",
+    "text":"One can be a good manager without having a precise answer to every question that a subordinate may raise about his or her work.",
+    "options":[
+      "strongly agree",
+      "agree",
+      "undecided",
+      "disagree",
+      "strongly disagree"
+    ]
+  },
+  {
+    "intro":"To what extent do you agree or disagree with the following statement?",
+    "text":"Persistent efforts are the surest way to results.",
+    "options":[
+      "strongly agree",
+      "agree",
+      "undecided",
+      "disagree",
+      "strongly disagree"
+    ]
+  },
+  {
+    "intro":"To what extent do you agree or disagree with the following statement?",
+    "text":"An organization structure in which certain subordinates have two bosses should be avoided at all cost.",
+    "options":[
+      "strongly agree",
+      "agree",
+      "undecided",
+      "disagree",
+      "strongly disagree"
+    ]
+  },
+  {
+    "intro":"To what extent do you agree or disagree with the following statement?",
+    "text":"A company's or organization's rules should not be broken - not even when the employee thinks breaking the rule would be in the organization's best interest.",
+    "options":[
+      "strongly agree",
+      "agree",
+      "undecided",
+      "disagree",
+      "strongly disagree"
+    ]
+  }
+]
